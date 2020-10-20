f74e3a942894b73486d095609df2c356d2e4b58b
Lex Ortiz <lex.ortizc@gmail.com>
2020-10-19 19:54:02 -0600

    JS: Render de Pizza, Bebidas y Postres

--- js/app.js
+++ js/app.js
@@ -1,30 +1,34 @@
-import data from '../data/menu.js';
-
 const prodMenuItem = document.querySelectorAll('.prod-menu-item');
 const sectionCombo = document.querySelector("#products-combos");
 const sectionPizza = document.querySelector("#products-pizza");
-const sectionBeveraage = document.querySelector("#products-beverages");
+const sectionBeverage = document.querySelector("#products-beverages");
 const sectionDessert = document.querySelector("#products-desserts");
 
-prodMenuItem[0].addEventListener('click', () => {
-  activeItem(prodMenuItem[0]);
-  showSecction(sectionCombo);
-});
+const renderMenu = (menu) => {
+  menu.combo.forEach( combo => {
+    sectionCombo.insertAdjacentHTML('beforeend', comboCardTemplate(combo));
+  });
 
-prodMenuItem[1].addEventListener('click', () => {
-  activeItem(prodMenuItem[1]);
-  showSecction(sectionPizza);
-});
+  menu.pizza.forEach( pizza => {
+    sectionPizza.insertAdjacentHTML('beforeend', foodCardTemplate(pizza));
+  });
 
-prodMenuItem[2].addEventListener('click', () => {
-  activeItem(prodMenuItem[2]);
-  showSecction(sectionBeveraage);
-});
+  menu.beverage.forEach( beverage => {
+    sectionBeverage.insertAdjacentHTML('beforeend', foodCardTemplate(beverage));
+  });
 
-prodMenuItem[3].addEventListener('click', () => {
-  activeItem(prodMenuItem[3]);
-  showSecction(sectionDessert);
-});
+  menu.dessert.forEach( dessert => {
+    sectionDessert.insertAdjacentHTML('beforeend', foodCardTemplate(dessert));
+  });
+}
+
+const getData = () => {
+  fetch('../data/menu.json')
+    .then( res => res.json())
+    .then( res => renderMenu(res.menu))
+}
+
+getData();
 
 const activeItem = (item) => {
   prodMenuItem.forEach( element => {
@@ -33,13 +37,23 @@ const activeItem = (item) => {
   item.classList.add('tab-active');
 }
 
-const showSecction = (section) => {
+const showSection = (section) => {
   sectionCombo.classList.remove('product-show');
   sectionPizza.classList.remove('product-show');
-  sectionBeveraage.classList.remove('product-show');
+  sectionBeverage.classList.remove('product-show');
   sectionDessert.classList.remove('product-show');
   section.classList.add('product-show');
 }
+
+const showProductSection = (prodMenuItem, sectionMenu) => {
+  activeItem(prodMenuItem);
+  showSection(sectionMenu);
+}
+
+prodMenuItem[0].addEventListener('click', () => showProductSection(prodMenuItem[0], sectionCombo) );
+prodMenuItem[1].addEventListener('click', () => showProductSection(prodMenuItem[1], sectionPizza) );
+prodMenuItem[2].addEventListener('click', () => showProductSection(prodMenuItem[2], sectionBeverage) );
+prodMenuItem[3].addEventListener('click', () => showProductSection(prodMenuItem[3], sectionDessert) );
 
 
 const comboCardTemplate = (combo) => {
@@ -65,6 +79,19 @@ const comboCardTemplate = (combo) => {
   </article>`
 }
 
-data.menu.combo.forEach( c => {
-  sectionCombo.insertAdjacentHTML('beforeend', comboCardTemplate(c));
-});
+const foodCardTemplate = (food) => {
+  if(food.name !== food.description) {
+    food.name = `${food.name} (${food.description})`
+  }
+
+  return `<article class="product flex">
+    <img src="https://images.unsplash.com/photo-1574126154517-d1e0d89ef734?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80" alt="pizza">
+    <div id="product-text" class="flex">
+      <div class="flex">
+        <h3>${food.name}</h3>
+        <h4 class="px-8">$${food.price}</h4>
+      </div>
+      <button class="btn">Agregar</button>
+    </div>
+  </article>`
+}
